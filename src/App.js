9e46616b9bd6c338a0e0ccc8e82364d971f20d41
Michael <mikek222@gmail.com>
2025-04-07 16:31:19 -0400

    added actions, reducer and types files, with early carveout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React, { useReducer, useEffect } from "react";
+import { initialState , reducer } from "./state/reducer";
+import { NEW_MESSAGE } from "./state/types";
+import { newMessage } from "./state/actions";
+
 
 function App() {
+
+const [state, dispatch] = useReducer(reducer, initialState)
+
+useEffect(()=>{
+  setInterval(() => dispatch(newMessage('foo')),
+  3000
+);
+}, []);
+
+console.log('state', state)
+
   return (
     <div >
      <h2>Reaction</h2>
---
+++ src/state/actions.js
@@ -0,0 +1,7 @@
+import { NEW_MESSAGE } from "./types";
+import { v4 as uuid } from 'uuid';
+
+export const newMessage = (text) => ({
+    type: NEW_MESSAGE,
+    item: { id: uuid(), text, timestamp: Date.now() }
+});
